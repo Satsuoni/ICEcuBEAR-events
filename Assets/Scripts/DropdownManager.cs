2c4aedbe91074b45f71f04d1331138f28bbcbf9e
Satsuoni <satsuoni@hotmail.com>
2019-06-21 22:46:45 +0900

    Seems to work

--- Assets/Scripts/DropdownManager.cs
+++ Assets/Scripts/DropdownManager.cs
@@ -15,14 +15,72 @@ public class DropdownManager : MonoBehaviour
             Destroy(this);
             return;
         }
+        managed.onValueChanged.AddListener(delegate
+        {
+            DropdownValueChanged(managed);
+        });
         //Dropdown.OptionData opt;
         //opt.image = null;
 
     }
-
+    void Awake()
+    {
+        Utilz.eventListUpdated += UpdateList;
+    }
+    void OnDestroy()
+    {
+        Utilz.eventListUpdated -= UpdateList;
+    }
     // Update is called once per frame
+    List<eventDesc> curOptions = new List<eventDesc>();
+    public void UpdateList()
+
+    {
+    
+        if (EventRestAPI.Instance == null)
+        {
+            Debug.Log("NULL Instance");
+            return;
+        }
+        curOptions.Clear();
+        List<Dropdown.OptionData> lst = new List<Dropdown.OptionData>();
+        if(EventRestAPI.settings==null)
+        {
+            Debug.Log("Nyanya NULL settings");
+            return;
+        }
+        if (EventRestAPI.settings.eventData == null)
+        {
+            Debug.Log("Nyanya NULL evdata");
+            return;
+        }
+        foreach (SavedEventData edat in EventRestAPI.settings.eventData)
+        {
+            Dropdown.OptionData dopt = new Dropdown.OptionData();
+            //Debug.Log("Nyanya"+JsonUtility.ToJson(edat.description));
+            dopt.image = null;
+            dopt.text = string.Format("Ev. {0}/{1} : {2} ({3}), {4}KJ Status:{5}",edat.description.run,edat.description.evn,edat.description.baseDesc,edat.description.eventDate,edat.description.energy,edat.status);
+            lst.Add(dopt);
+            curOptions.Add(edat.description);
+        }
+        //Debug.Log("Nyanya options");
+        //Debug.Log("DropboxUpdate");
+        if(managed==null)
+            managed = gameObject.GetComponent<Dropdown>();
+        managed.options = lst;
+        //managed.ClearOptions();
+        //managed.AddOptions(lst);
+    }
     void Update()
     {
         
+    }
+    void DropdownValueChanged(Dropdown change)
+    {
+        //Debug.LogFormat("New val :{0}",change.value);
+        if(change.value<curOptions.Count)
+        {
+            EventRestAPI.Instance.processNewNotification(curOptions[change.value], true);
+        }
     }
 }
