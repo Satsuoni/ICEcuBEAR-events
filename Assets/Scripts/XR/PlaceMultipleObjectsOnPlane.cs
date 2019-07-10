1410744ea8dbce6b8349506b0c5d72f58ab10204
Satsuoni <satsuoni@hotmail.com>
2019-07-10 23:13:04 +0900

    transiting

---
+++ Assets/Scripts/XR/PlaceMultipleObjectsOnPlane.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.EventSystems;
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceMultipleObjectsOnPlane : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    GameObject m_PlacedPrefab;
+
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public GameObject placedPrefab
+    {
+        get { return m_PlacedPrefab; }
+        set { m_PlacedPrefab = value; }
+    }
+
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedObject { get; private set; }
+
+    /// <summary>
+    /// Invoked whenever an object is placed in on a plane.
+    /// </summary>
+    public static event Action onPlacedObject;
+
+    ARRaycastManager m_RaycastManager;
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+    }
+    List<GameObject> instObjects = new List<GameObject>();
+    public void clearObjects()
+    {
+        foreach (GameObject obj in instObjects)
+        {
+            Destroy(obj);
+        }
+        instObjects.Clear();
+    }
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+            {
+                return;
+            }
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+                {
+                    Pose hitPose = s_Hits[0].pose;
+
+                    spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
+                    instObjects.Add(spawnedObject);
+                    while (instObjects.Count > 3)
+                    {
+                        var obj = instObjects[0];
+                        instObjects.RemoveAt(0);
+                        Destroy(obj);
+                    }
+                    if (onPlacedObject != null)
+                    {
+                        onPlacedObject();
+                    }
+                }
+            }
+        }
+    }
+}
